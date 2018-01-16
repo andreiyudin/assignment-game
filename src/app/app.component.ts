82f4fc8ae3105181592fdb769d7b4ba0627aa863
Andrei Yudin <yudin345@yandex.ru>
2018-01-15 22:11:45 -0500

    first deploying of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	oddNumbers: number[] = []; //creating new array
+	evenNumbers: number[] = []; //creating new array
+
+  onIntervalFired(firedNumber: number) {
+  	// console.log(firedNumber);
+  	if (firedNumber % 2  === 0 ) {
+  		this.evenNumbers.push(firedNumber); //adding new in array
+  	} else {
+  		this.oddNumbers.push(firedNumber); //adding new in array
+  	}
+  }
+
 }
